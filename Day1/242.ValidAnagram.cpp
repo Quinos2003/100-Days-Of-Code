66aea8c1aa65d8b4afaca99d0bf2e68705b799ff
Quinos2003 <asthana.samarth2@gmail.com>
2023-08-06 05:07:37 +0530

    alternate solution by hashmap added

--- Day1/242.ValidAnagram.cpp
+++ Day1/242.ValidAnagram.cpp
@@ -5,5 +5,21 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         return s == t;
+
+        // alternate solution by hashmap 
+        // if(s.size() != t.size()) return false;
+        
+        // unordered_map<char,int> smap;
+        // unordered_map<char,int> tmap;
+        
+        // for(int i = 0; i < s.size(); i++){
+        //     smap[s[i]]++;
+        //     tmap[t[i]]++;
+        // }
+        
+        // for(int i = 0; i < smap.size(); i++){
+        //     if(smap[i] != tmap[i]) return false;
+        // }
+        // return true;
     }
 };
