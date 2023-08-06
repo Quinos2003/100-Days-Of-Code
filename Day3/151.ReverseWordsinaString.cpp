51f56d9862fcc8adb4d948597de4c57ebee8d436
Quinos2003 <asthana.samarth2@gmail.com>
2023-08-07 03:44:04 +0530

    Stored Individual words in stack and then popped them to create a reverse words string

---
+++ Day3/151.ReverseWordsinaString.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        stack <string> st;
+        int n = s.size();
+        string temp = "";
+        string ans = "";
+
+        for(int i =0; i <n ; i ++){
+            char ch= s[i];
+            if(ch==' '){
+                if(temp!=""){
+                    st.push(temp);
+                    temp="";
+                }
+                
+            }
+            else
+            temp = temp + ch;
+        }
+        if(temp!="")
+        st.push(temp);
+
+        while(!st.empty()){
+            ans = ans + (st.top()+" ");
+            st.pop();
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
+ 
+// https://leetcode.com/problems/reverse-words-in-a-string/submissions/1014192882/
