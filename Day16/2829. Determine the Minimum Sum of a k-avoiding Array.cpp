f2a589a580e8f33a16fa9f91919ec8b1e4babed8
Quinos2003 <asthana.samarth2@gmail.com>
2023-08-20 23:10:51 +0530

    Day 16 Progress

---
+++ Day16/2829. Determine the Minimum Sum of a k-avoiding Array.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+    
+//     int minimumSum(int n, int k) {
+//         int sum =0;
+//         unordered_map<int, int> mp;
+//         int index =0 , num=1;
+//         while(index<n){
+//             if(mp.find(k - num) == mp.end()){
+//                 sum+=num;
+//                 mp[num]=1;
+//             }
+//             num++;
+//             index++;
+//         }
+//         return sum;
+//     }
+// };
+class Solution {
+public:
+    int minimumSum(int n, int k) {
+        int sum = 0;
+        unordered_set<int> cantUse; 
+
+        int index = 0, num = 1;
+        while (index < n) {
+            if (cantUse.find(num) == cantUse.end()) {
+                sum += num;
+                cantUse.insert(k - num); 
+                index++;
+            }
+            num++;
+        }
+        return sum;
+    }
+};
