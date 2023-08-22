762a13462a316d8aee0920aa437b4f6d683200e9
Quinos2003 <asthana.samarth2@gmail.com>
2023-08-22 23:49:52 +0530

    Day 18 Iterative Solution

--- Day18/235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Day18/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -14,9 +14,21 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL) return NULL;
 
-        if(root->val > p->val && root->val>q->val) return lowestCommonAncestor(root->left ,p,q );
-        if(root->val < p->val && root->val<q->val) return lowestCommonAncestor(root->right ,p,q );
-        return root;
+        // if(root->val > p->val && root->val>q->val) return lowestCommonAncestor(root->left ,p,q );
+        // if(root->val < p->val && root->val<q->val) return lowestCommonAncestor(root->right ,p,q );
+        // return root;
         // RECURSIVE APPROACH
+        TreeNode *ans = root;
+
+        while(1<2){
+            if(p->val < ans->val && q->val < ans->val)
+            ans = ans->left;
+
+            else if(p->val > ans->val && q->val >ans->val)
+            ans = ans->right;
+            else
+            break;
+        }
+        return ans;
     }
 };
