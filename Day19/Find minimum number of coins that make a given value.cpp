65f690f5ca89d6539603f54369496d9beb58e294
Quinos2003 <asthana.samarth2@gmail.com>
2023-08-23 23:54:29 +0530

    Day 19 DP Memoization Approach

--- Day19/Find minimum number of coins that make a given value.cpp
+++ Day19/Find minimum number of coins that make a given value.cpp
@@ -35,45 +35,116 @@ using namespace std;
 // of coins to make a given change V
 
 // m is size of coins array (number of different coins)
-int minCoins(int coins[], int m, int V)
+// int minCoins(int coins[], int m, int V)
+// {
+// 	// table[i] will be storing the minimum number of coins
+// 	// required for i value. So table[V] will have result
+// 	int table[V + 1];
+
+// 	// Base case (If given value V is 0)
+// 	table[0] = 0;
+
+// 	// Initialize all table values as Infinite
+// 	for (int i = 1; i <= V; i++)
+// 		table[i] = INT_MAX;
+
+// 	// Compute minimum coins required for all
+// 	// values from 1 to V
+// 	for (int i = 1; i <= V; i++) {
+// 		// Go through all coins smaller than i
+// 		for (int j = 0; j < m; j++)
+// 			if (coins[j] <= i) {
+// 				int sub_res = table[i - coins[j]];
+// 				if (sub_res != INT_MAX
+// 					&& sub_res + 1 < table[i])
+// 					table[i] = sub_res + 1;
+// 			}
+// 	}
+
+// 	if (table[V] == INT_MAX)
+// 		return -1;
+
+// 	return table[V];
+// }
+// Driver program to test above function
+// int main()
+// {
+// 	int coins[] = { 9, 6, 5, 1 };
+// 	int m = sizeof(coins) / sizeof(coins[0]);
+// 	int V = 11;
+// 	cout << "Minimum coins required is "
+// 		<< minCoins(coins, m, V);
+// 	return 0;
+// }
+
+// Coin change using the Top Down (Memoization) Dynamic Programming: 
+
+int minCoinsUtil(int coins[], int m, int V, int* dp)
 {
-	// table[i] will be storing the minimum number of coins
-	// required for i value. So table[V] will have result
-	int table[V + 1];
+	// Base case: If target value V is 0, no coins are
+	// needed
+	if (V == 0)
+		return 0;
 
-	// Base case (If given value V is 0)
-	table[0] = 0;
+	// If subproblem is already solved, return the result
+	// from DP table
+	if (dp[V] != -1)
+		return dp[V];
 
-	// Initialize all table values as Infinite
-	for (int i = 1; i <= V; i++)
-		table[i] = INT_MAX;
+	int res = INT_MAX;
 
-	// Compute minimum coins required for all
-	// values from 1 to V
-	for (int i = 1; i <= V; i++) {
-		// Go through all coins smaller than i
-		for (int j = 0; j < m; j++)
-			if (coins[j] <= i) {
-				int sub_res = table[i - coins[j]];
-				if (sub_res != INT_MAX
-					&& sub_res + 1 < table[i])
-					table[i] = sub_res + 1;
-			}
+	// Iterate over all coins and recursively solve for
+	// subproblems
+	for (int i = 0; i < m; i++) {
+		if (coins[i] <= V) {
+			// Recursive call to solve for remaining value V
+			// - coins[i]
+			int sub_res
+				= minCoinsUtil(coins, m, V - coins[i], dp);
+
+			// If the subproblem has a valid solution and
+			// the total number of coins is smaller than the
+			// current result, update the result
+			if (sub_res != INT_MAX && sub_res + 1 < res)
+				res = sub_res + 1;
+		}
 	}
 
-	if (table[V] == INT_MAX)
-		return -1;
+	// Save the result in the DP table
+	dp[V] = res;
 
-	return table[V];
+	return res;
 }
 
-// Driver program to test above function
+// Function to find the minimum number of coins needed to
+// make a target value
+int minCoins(int coins[], int m, int V)
+{
+	// Create a DP table to store results of subproblems
+	int* dp = new int[V + 1];
+	memset(dp, -1,
+		sizeof(int)
+			* (V + 1)); // Initialize DP table with -1
+
+	// Call the utility function to solve the problem
+	return minCoinsUtil(coins, m, V, dp);
+}
+
+// Driver code
 int main()
 {
 	int coins[] = { 9, 6, 5, 1 };
 	int m = sizeof(coins) / sizeof(coins[0]);
 	int V = 11;
-	cout << "Minimum coins required is "
-		<< minCoins(coins, m, V);
+
+	int res = minCoins(coins, m, V);
+	if (res == INT_MAX)
+		res = -1;
+
+	// Find the minimum number of coins required
+	cout << "Minimum coins required is " << res;
+
 	return 0;
 }
+
+
