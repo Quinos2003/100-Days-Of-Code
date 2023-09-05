536d8e2d031f5154d578a5abe6eaf1f4e96f6ab7
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-05 23:59:47 +0530

    Day 32 Progress

--- Day32/230. Kth Smallest Element in a BST.cpp
+++ Day32/230. Kth Smallest Element in a BST.cpp
@@ -9,17 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> vSmallest;
+//         Smallestkth(root,k,vSmallest);
+//         return vSmallest[k-1];
+//     }
+//     void Smallestkth(TreeNode* root, int k,vector<int> &vSmallest)
+//     {
+//         if(root->left) Smallestkth(root->left,k,vSmallest);
+//         vSmallest.push_back(root->val);
+//         if(root->right) Smallestkth(root->right,k,vSmallest);
+//     }
+// };
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> vSmallest;
-        Smallestkth(root,k,vSmallest);
-        return vSmallest[k-1];
+    int kthSmallest(TreeNode* root, int k) { 
+        return dfs(root, k);
     }
-    void Smallestkth(TreeNode* root, int k,vector<int> &vSmallest)
-    {
-        if(root->left) Smallestkth(root->left,k,vSmallest);
-        vSmallest.push_back(root->val);
-        if(root->right) Smallestkth(root->right,k,vSmallest);
+    int dfs(TreeNode* node, int& k){
+        if(!node) return INT_MIN;
+        int l = dfs(node->left,k);
+        if(k == 0) return l;
+        else if(k == 1){
+            k--;
+            return node->val;
+        }
+        else{
+            k--;
+            int r = dfs(node->right,k);
+            if(k==0) return r;
+            else return INT_MIN;   
+        }
     }
+
 };
