4ed6273b9b37720166db8cca15cdaf1c1c796461
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-07 23:40:01 +0530

    Day 34 Progress

--- Day34/424. Longest Repeating Character Replacement.cpp
+++ Day34/424. Longest Repeating Character Replacement.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int i =0 , j=0 , ans =0;
+        int maxC=0;
+        
+        while(j < s.length()){
+            mp[s[j]]++;
+            maxC = max( maxC , mp[s[j]]);
+
+            int window = j-i+1;
+
+            if( window - maxC > k ){ // meaning window size shrink i++
+                mp[s[i]]--;
+                i++;
+            }
+            window =j-i+1;
+            ans = max ( ans , window);
+            j++;
+        }
+        return ans;
+    }
+};
