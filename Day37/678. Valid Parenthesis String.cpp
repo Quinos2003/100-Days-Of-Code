6949ac28a316e9148ec9bf21d7612567e5eb6062
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-10 23:48:28 +0530

    Day 37 Progress

---
+++ Day37/678. Valid Parenthesis String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int balance =0;
+        for (auto it : s){
+            if(it==')') balance --;
+            else balance ++;
+
+            if(balance<0) return false;
+        }
+        if(balance==0) return true; // meaning all * used 
+
+        balance =0;
+
+        for(int i =s.length() -1; i >=0; i--){
+            if(s[i]=='(') balance --;
+            else balance ++;
+
+            if(balance<0) return false;
+        }
+
+        return true;
+    }
+};
