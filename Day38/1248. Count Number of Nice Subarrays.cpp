69f2236c2929c3223e5a1e80618b045eb7e8529c
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-11 23:19:19 +0530

    Day 38 Progress

---
+++ Day38/1248. Count Number of Nice Subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int atMost(vector<int>& nums , int k ){
+    int ans =0, i =0 , j =0 ;
+    while(j<nums.size()){
+        k-= nums[j]%2;
+        while(k<0){
+            k+= nums[i++]%2;
+        }
+        ans += j-i+1;
+        j++;
+    }
+    return ans;
+}
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
