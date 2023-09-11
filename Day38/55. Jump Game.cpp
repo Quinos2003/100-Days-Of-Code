74f5e9dfe73cbc156cd91c6d31145ba5c39f6d67
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-11 23:39:32 +0530

    Day 38 Progress

--- Day38/55. Jump Game.cpp
+++ Day38/55. Jump Game.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-
-    bool canJump(vector<int>& nums) {
-    int farest = 0;
-    for(int i = 0;i < nums.size(); i++){
-        if(farest < i) return false;
-        farest = max(i + nums[i], farest);
-    }
-    return true;
-    }
-};
     // bool canJump(vector<int>& nums) {
         // int p1 =0;
         // while(p1<nums.size()){
@@ -24,3 +14,12 @@ public:
         // }
         // return true;
     // }
+    bool canJump(vector<int>& nums) {
+    int farest = 0;
+    for(int i = 0;i < nums.size(); i++){
+        if(farest < i) return false;
+        farest = max(i + nums[i], farest);
+    }
+    return true;
+    }
+};
