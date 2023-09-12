2c72b7b3cfa16a7f7b3bba81a2f00ed252ccc850
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-12 23:49:44 +0530

    Day 39 Progress

--- Day39/231. Power of Two.cpp
+++ Day39/231. Power of Two.cpp
@@ -1,1 +1,17 @@
-
+// 231. Power of Two
+class Solution {
+public:
+    // bool isPowerOfTwo(int n) {
+    //     if(n==0) return false;
+    //     while(n%2==0) n/=2;
+    //     return n==1;
+    // }
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        return (n&n-1)==0;
+    }
+    // bool isPowerOfTwo(int n) {
+    //     if(n==0) return false;
+    //     return (n>0)&&(1073741824%n==0);
+    // }
+};
