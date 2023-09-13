6d6df433dfbe990c9ab8619cfccccc7ee01a8cb6
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-13 23:23:21 +0530

    Day 40 Progress

---
+++ Day40/2220. Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int value = start^goal , count=0;
+        // now value has 1's that are different in both
+        while(value!=0)
+        {
+            value&=value-1;
+            count++;
+        }
+        return count;
+    }
+};
