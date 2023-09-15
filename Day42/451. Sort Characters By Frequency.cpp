3c3d29de5dc22e6f7c1088913e49095635f685a0
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-15 23:56:15 +0530

    Day 42 Progress

---
+++ Day42/451. Sort Characters By Frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        priority_queue<pair<int,char>> maxheap;
+        for(auto x : s)   mp[x]++;
+    
+        for(auto it : mp )  maxheap.push({it.second , it.first});
+
+        s="";
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second);
+            maxheap.pop();
+        }
+        return s;
+    }
+};
