c636ac91e1a73d1ecf88ae0b18131f6402e1645d
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-23 23:59:14 +0530

    Day 50 Progress

--- Day50/198. House Robber.cpp
+++ Day50/198. House Robber.cpp
@@ -3,9 +3,7 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n<2) return nums[0];
-
         vector<int> dp(n+1);
-
         dp[0]=0;
         dp[1]= nums[0];
         dp[2]= max(nums[0],nums[1]);
@@ -14,6 +12,5 @@ public:
             dp[i] =  max(dp[i-3]+ nums[i-1],dp[i-2] + nums[i-1]) ;
         }
         return max(dp[n],dp[n-1]);
-
     }
 };
