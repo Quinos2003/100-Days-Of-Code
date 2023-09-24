bb9e76cbc5fc81df8aee0d482c9fe5a5ba00f6ec
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-24 23:49:50 +0530

    Day 50 Progress

--- Day50/198. House Robber.cpp
+++ Day50/198. House Robber.cpp
@@ -2,15 +2,32 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n<2) return nums[0];
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]= nums[0];
-        dp[2]= max(nums[0],nums[1]);
-        
-        for(int i =3 ; i < n+1; i ++){
-            dp[i] =  max(dp[i-3]+ nums[i-1],dp[i-2] + nums[i-1]) ;
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for(int i =1 ; i < n ; i++){
+            int take = nums[i];
+            if(i>1) take += prev2;
+            int curi = max(take, prev);
+
+            prev2=prev;
+            prev = curi;
         }
-        return max(dp[n],dp[n-1]);
+        return prev;
     }
 };
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n<2) return nums[0];
+//         vector<int> dp(n+1);
+//         dp[0]=0;
+//         dp[1]= nums[0];
+//         dp[2]= max(nums[0],nums[1]);
+//         for(int i =3 ; i < n+1; i ++){
+//             dp[i] =  max(dp[i-3]+ nums[i-1],dp[i-2] + nums[i-1]) ;
+//         }
+//         return max(dp[n],dp[n-1]);
+//     }
+// };
