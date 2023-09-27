4500563b35b06c543af4a7874e1982092faaea66
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-27 23:56:48 +0530

    Day 54 Progress

--- Day54/416. Partition Equal Subset Sum.cpp
+++ Day54/416. Partition Equal Subset Sum.cpp
@@ -8,29 +8,29 @@ public:
         if(Totsum%2==1) return false;
 
         else {
-        int k = Totsum / 2;
-        vector<bool> prev(k + 1, false);
-        prev[0] = true;
+            int k = Totsum / 2;
+            vector<bool> prev(k + 1, false);
+            prev[0] = true;
 
-        if (nums[0] <= k)
-            prev[nums[0]] = true;
+            if (nums[0] <= k)
+                prev[nums[0]] = true;
 
-        for (int ind = 1; ind < n; ind++) {
+            for (int ind = 1; ind < n; ind++) {
 
-            vector<bool> cur(k + 1, false);
-            cur[0] = true;
+                vector<bool> cur(k + 1, false);
+                cur[0] = true;
 
-            for (int target = 1; target <= k; target++) {
-                bool notTaken = prev[target];
-                bool taken = false;
-                
-                if (nums[ind] <= target)  taken = prev[target - nums[ind]];
+                for (int target = 1; target <= k; target++) {
+                    bool notTaken = prev[target];
+                    bool taken = false;
 
-                cur[target] = notTaken || taken;
+                    if (nums[ind] <= target)  taken = prev[target - nums[ind]];
+
+                    cur[target] = notTaken || taken;
+                }
+                prev = cur;
             }
-            prev = cur;
-        }
-        return prev[k];
+            return prev[k];
         }
     }
 };
