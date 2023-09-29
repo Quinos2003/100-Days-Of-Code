d9e185a3a849035fdd87eddfab26f295847cfe6b
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-29 23:56:57 +0530

    Day 56 Progress

---
+++ Day56/380. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,31 @@
+class RandomizedSet {
+public:
+unordered_map<int, int> mp;
+vector<int> arr;
+    RandomizedSet() {
+        return;
+    }
+    bool insert(int val) {
+        if(mp.count(val)) return false;
+        arr.push_back(val);
+        mp[val] = arr.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mp.count(val)){
+            int idx = mp[val];
+            int last_val = arr.back();
+            arr[idx] = last_val;
+            arr.pop_back();
+            mp[last_val] = idx;
+            mp.erase(val);
+            return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+        return arr[rand() % arr.size()];
+    }
+};
