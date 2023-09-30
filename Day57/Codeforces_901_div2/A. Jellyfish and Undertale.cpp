bcbfbfd17345e690adffe5668fc525aaa9e8eee1
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-30 23:55:01 +0530

    Day 57 Progress

--- Day57/Codeforces_901_div2/A. Jellyfish and Undertale.cpp
+++ Day57/Codeforces_901_div2/A. Jellyfish and Undertale.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int b,a,n;
-    cin>>b>>a>>n;
-    vector<int> tool(n);
-    for( int ttt =0 ; ttt < n ; ttt++){
-        cin>>tool[ttt];
+int explodeTime(int a , int b ,  vector<int>& tool){
+    int sum =0;
+    for( int it : tool ){
+        sum+= min(a,it);
     }
-    
+    return b+sum-1;
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<int> ans;
+    for(int i = 0; i<t; i++){
+        int b,a,n;
+        cin>>a>>b>>n;
+        vector<int> tool(n);
+        for( int ttt =0 ; ttt < n ; ttt++){
+            cin>>tool[ttt];
+        }
+        ans.push_back(explodeTime(a,b,tool));
+    }
+    for( int i =0 ; i < t ; i ++){
+        cout<<ans[i]<<endl;
+    }
     return 0;
 }
