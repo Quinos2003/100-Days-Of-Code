7bfa2de3965877e44926ad8c07496a52455a2c28
Quinos2003 <asthana.samarth2@gmail.com>
2023-09-30 22:01:00 +0530

    Day 57 Progress

---
+++ Day57/Codeforces_901_div2/A. Jellyfish and Undertale.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int b,a,n;
+    cin>>b>>a>>n;
+    vector<int> tool(n);
+    for( int ttt =0 ; ttt < n ; ttt++){
+        cin>>tool[ttt];
+    }
+    
+    return 0;
+}
