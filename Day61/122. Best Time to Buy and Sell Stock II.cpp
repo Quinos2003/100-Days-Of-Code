0ab03703b191ee99f9a415b5d0e1b2e872015f38
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-04 23:58:02 +0530

    Day 61 Progress

--- Day61/122. Best Time to Buy and Sell Stock II.cpp
+++ Day61/122. Best Time to Buy and Sell Stock II.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         // int n = prices.size();
-        // if(n<2) return 0; // if else use 
+        // if(n<2) return 0; 
         int profit =0;
         // int prev = prices[0], curr=prices[1] ;
         for(int i=0 ; i < prices.size()-1 ; i++){
