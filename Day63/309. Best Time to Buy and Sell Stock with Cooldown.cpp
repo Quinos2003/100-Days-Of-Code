611075a3b3cfab910b6ddf1f3d4b143c24fba609
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-06 23:36:28 +0530

    Day 63 Progress

--- Day63/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ Day63/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -36,6 +36,18 @@ int maxProfit(vector<int> &prices) {
     
     return cur[0]; // Return the max profit for buying.
 }
+// // did not understand this approach 
+    // int maxProfit(vector<int>& prices) {
+    //     int sold = 0, hold = INT_MIN, rest = 0;
+    //     for (int i=0; i<prices.size();i++)
+    //     {
+    //         int prvSold = sold;
+    //         sold = hold + prices[i];
+    //         hold = max(hold, rest-prices[i]);
+    //         rest = max(rest, prvSold);
+    //     }
+    //     return max(sold, rest);
+    // }
 int main() {
     vector<int> prices {4, 9, 0, 4, 10};
                                  
