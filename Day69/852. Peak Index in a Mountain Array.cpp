41d4c58d5d4c079446186654eaef7a589318979c
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-12 23:45:58 +0530

    Day 69 Progress

--- Day69/852. Peak Index in a Mountain Array.cpp
+++ Day69/852. Peak Index in a Mountain Array.cpp
@@ -1,7 +1,21 @@
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int> A) {
+//         for (int i = 1; i + 1 < A.size(); ++i) if (A[i] > A[i + 1]) return i;
+//         return 0;
+//     }
+// };
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int> A) {
-        for (int i = 1; i + 1 < A.size(); ++i) if (A[i] > A[i + 1]) return i;
-        return 0;
+        int l=0, r = A.size()-1, mid;
+        while(l<r){
+            mid= l+ (r-l)/2;
+
+            if(A[mid]<A[mid+1]) l= mid +1;
+
+            else r = mid;
+        }
+        return l;
     }
 };
