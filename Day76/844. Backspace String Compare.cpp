68de7d2f7813b532ceab4e0b2e6aab3a74e4160c
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-19 23:51:36 +0530

    Day 76 Progress

--- Day76/844. Backspace String Compare.cpp
+++ Day76/844. Backspace String Compare.cpp
@@ -1,17 +1,43 @@
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+        
+//         int i = s.length()-1, j = t.length()-1;
+//         int count1 =0 , count2 =0;
+
+//         while(i>= 0 || j>=0 ){
+//             while(i>=0 && (s[i]== '#' || count1>0)) s[i--]=='#' ? count1++ : --count1;
+//             while(j>=0 && (t[j]== '#' || count2>0)) t[j--]=='#' ? count2++ : count2--;
+//             if(i<0 || j<0) return i==j;
+//             if(s[i--] != t[j--]) return false;
+            
+//         }
+//         return i==j;
+//     }
+// };
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
-        
-        int i = s.length()-1, j = t.length()-1;
-        int count1 =0 , count2 =0;
+    void stringDiscardBackspace(string& str) {
+        string tmp;
 
-        while(i>= 0 || j>=0 ){
-            while(i>=0 && (s[i]== '#' || count1>0)) s[i--]=='#' ? count1++ : --count1;
-            while(j>=0 && (t[j]== '#' || count2>0)) t[j--]=='#' ? count2++ : count2--;
-            if(i<0 || j<0) return i==j;
-            if(s[i--] != t[j--]) return false;
-            
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '#') {
+                if (tmp.length()) {
+                    tmp.pop_back();
+                }
+                
+            } else {
+                tmp.push_back(str[i]);
+            }
         }
-        return i==j;
+
+        str = tmp;
+    }
+
+    bool backspaceCompare(string s, string t) {
+        stringDiscardBackspace(s);
+        stringDiscardBackspace(t);
+
+        return (s == t);
     }
 };
