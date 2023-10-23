58c5c77479109270fe15d8ab6dd944af3df52765
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-23 23:51:21 +0530

    Day 80 Progress

--- Day80/342. Power of Four.cpp
+++ Day80/342. Power of Four.cpp
@@ -1,11 +1,21 @@
+// class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//         while(n>1){
+//             if(n%4!=0) return false;
+//             n/=4;
+//         }
+//         return n == 1;
+//     }
+// };
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        while(n>1){
-            if(n%4!=0) return false;
-            n/=4;
+        if(n<0) return false;
+
+        for(int i =0 ; i < 32 ; i +=2){
+            if(n == 1<<i) return true; // basically checked all 4^1 to 4^32
         }
-        return n == 1;
+        return false;
     }
 };
-
