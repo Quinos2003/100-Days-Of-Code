9a731588cbfe29f3e71ed990fcc0241f78786afd
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-23 23:52:29 +0530

    Day 80 Progress

---
+++ Day80/Duplicate subtree in Binary Tree.cpp
@@ -0,0 +1,36 @@
+// Duplicate subtree in Binary Tree
+
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+  public:
+    /*This function returns true if the tree contains 
+    a duplicate subtree of size 2 or more else returns false*/
+ map<string, int>mp;
+ string solve(Node* root){
+        if(!root) return "$";
+        string s="";
+        if(!root->right and !root->left){
+            s+=to_string(root->data);
+            return s;
+        }
+        s+=to_string(root->data);
+        s+='/';
+        s+=solve(root->left);
+        s+=solve(root->right);
+        mp[s]++;
+        return s;
+    }
+    int dupSub(Node *root) {
+        solve(root);
+        for(auto it:mp)
+        if(it.second>=2) return true;
+        return false;
+    }
+};
