1a25937d61411e9d95917c9a7b790805a83d6a65
Quinos2003 <asthana.samarth2@gmail.com>
2023-10-26 00:00:15 +0530

    Day 82 Progress

--- Day82/779. K-th Symbol in Grammar.cpp
+++ Day82/779. K-th Symbol in Grammar.cpp
@@ -1,8 +1,14 @@
+// class Solution {
+// public:
+//     int kthGrammar(int N, int K) {
+//         int n;
+//         for (n = 0, K -= 1; K ; K &= (K - 1)) n++;
+//         return n & 1;
+//     }
+// };
 class Solution {
 public:
     int kthGrammar(int N, int K) {
-        int n;
-        for (n = 0, K -= 1; K ; K &= (K - 1)) n++;
-        return n & 1;
+        return __builtin_popcount(K - 1) & 1;
     }
 };
