26ee0a6f57cdfa9e90022fd610ffc2c71ccf6fcc
Quinos2003 <asthana.samarth2@gmail.com>
2023-11-04 23:46:29 +0530

    Day 92 Progress

---
+++ Day92/1503. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int res=0;
+        for(int it : left) res = max (res,it);
+        for(int it : right) res = max (res,n- it);
+        return res;
+    }
+};
