bdf4ec8025ff46910d5284fe6840e06d2d08797b
Quinos2003 <asthana.samarth2@gmail.com>
2023-11-06 23:28:51 +0530

    Day 94 Progress

--- Day94/1845. Seat Reservation Manager.cpp
+++ Day94/1845. Seat Reservation Manager.cpp
@@ -1,21 +1,37 @@
+// class SeatManager {
+// public:
+// set<int> s;
+//     SeatManager(int n) {
+//         for (int i = 1; i <= n; i++) s.insert(i);
+//     }
+    
+//     int reserve() {
+//         int res = *begin(s);
+//         s.erase(begin(s));
+//         return res;
+//     }
+    
+//     void unreserve(int seatNumber) {
+//         s.insert(seatNumber);
+//     }
+// };
+
 class SeatManager {
 public:
-set<int> s;
-    SeatManager(int n) {
-        for (int i = 1; i <= n; i++) s.insert(i);
-    }
-    
+priority_queue<int, vector<int>, greater<int>> pq;
+int max_s = 0;
+    SeatManager(int n) {}    
     int reserve() {
-        int res = *begin(s);
-        s.erase(begin(s));
+
+        int res = pq.empty() ? ++max_s : pq.top();
+
+        if (!pq.empty()) pq.pop();
         return res;
     }
-    
     void unreserve(int seatNumber) {
-        s.insert(seatNumber);
+        pq.push(seatNumber);
     }
 };
-
 
 /**
  * Your SeatManager object will be instantiated and called as such:
