12d867971d5f0db770f2b3274817fdcb8cb748c7
Quinos2003 <asthana.samarth2@gmail.com>
2023-11-07 23:51:46 +0530

    Day 95 Progress

---
+++ Day95/2375. Construct Smallest Number From DI String.cpp
@@ -0,0 +1,23 @@
+// When u see a 'D' or 'I' simply push the number to stack and 
+// when see 'I' add stack elements to answer.
+
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        stack<int> s;
+        string ans = "";
+        if(pattern[0] == 'I') ans += '1';
+        else s.push('1');
+        for(int i = 1; i <= pattern.size(); i++){
+            if(pattern[i-1] == 'I'){
+                while(!s.empty()){  
+                    ans += s.top(); 
+                    s.pop(); 
+                }
+            }
+            s.push('0' + i+1);
+        }
+        while(!s.empty()){ ans += s.top(); s.pop();  }
+        return ans;
+    }
+};
