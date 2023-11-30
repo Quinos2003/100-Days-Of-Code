fa3972ba02117d04a3e4e2aee8c7110dbfdf7ddd
Quinos2003 <asthana.samarth2@gmail.com>
2023-11-30 23:56:43 +0530

    Day 117 Progress

--- Day117/1611. Minimum One Bit Operations to Make Integers Zero.cpp
+++ Day117/1611. Minimum One Bit Operations to Make Integers Zero.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int minimumOneBitOperations(int n, int res = 0) {
-        if (n == 0) return res;
-        int b = 1;
-        while ((b << 1) <= n)
-            b = b << 1;
-        return minimumOneBitOperations((b >> 1) ^ b ^ n, res + b);
-    }
+    // int minimumOneBitOperations(int n, int res = 0) {
+    //     if (n == 0) return res;
+    //     int b = 1;
+    //     while ((b << 1) <= n)
+    //         b = b << 1;
+    //     return minimumOneBitOperations((b >> 1) ^ b ^ n, res + b);
+    // }
 
+    int minimumOneBitOperations(int n) {
+        int res;
+        for (res = 0; n > 0; n &= n - 1)
+            res = -(res + (n ^ (n - 1)));
+        return abs(res);
+    }
 };
