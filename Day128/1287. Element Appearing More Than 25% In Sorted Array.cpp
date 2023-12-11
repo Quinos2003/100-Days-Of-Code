cb0a63a6aa63a3fce626523b27387c4741f5be77
Quinos2003 <asthana.samarth2@gmail.com>
2023-12-12 00:02:58 +0530

    LeetCode Daily

--- Day128/1287. Element Appearing More Than 25% In Sorted Array.cpp
+++ Day128/1287. Element Appearing More Than 25% In Sorted Array.cpp
@@ -1,13 +1,31 @@
+// class Solution {
+// public:
+// 	int findSpecialInteger(vector<int>& arr) {
+// 		unordered_map<int, int> m;
+// 		for(int i = 0; i < arr.size(); i++){
+// 			m[arr[i]]++;
+// 		}
+// 		for(auto i : m){
+// 			if(i.second > arr.size() / 4){
+// 				return i.first;
+// 			}
+// 		}
+// 		return arr[0];
+// 	}
+// };
 class Solution {
 public:
 	int findSpecialInteger(vector<int>& arr) {
-		unordered_map<int, int> m;
-		for(int i = 0; i < arr.size(); i++){
-			m[arr[i]]++;
-		}
-		for(auto i : m){
-			if(i.second > arr.size() / 4){
-				return i.first;
+		int c = 0;
+		for(int i = 1; i < arr.size(); i++){
+			if(arr[i] == arr[i - 1]){
+				c++;
+			}
+			else{
+				c = 1;
+			}
+			if(c > arr.size() / 4){
+				return arr[i];
 			}
 		}
 		return arr[0];
